b8f0d3e9ec2cf0fdbe9af92ece62e3c5082a8e73
AnyhtingSoup <williamenglish1220@gmail.com>
2024-05-07 22:16:03 +0100

    Deleted

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd Application/src/CatEngine && g++ -c App.cpp -o App.o
-cd ../../../
-ar rcs libcatengine.a Application/src/CatEngine/App.o
-g++ Sandbox/src/SandboxApp.cpp -static -L. -lcatengine -o main.exe
-
-printf "Build Successful\n"
