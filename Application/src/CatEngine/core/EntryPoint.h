8539c8591677498e03f1fca84d30f0168d6e9bfe
AnyhtingSoup <williamenglish1220@gmail.com>
2024-05-07 22:19:22 +0100

    Added Log Example

--- Application/src/CatEngine/core/EntryPoint.h
+++ Application/src/CatEngine/core/EntryPoint.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include "Log.h"
+
 extern CatEngine::App* CatEngine::CreateApp();
 
 int main(int argc, char** argv)
 {
+
+    CatEngine::Log::Init();
+    API_CRITICAL("API Logger initialized");
+    CLI_INFO("CLI Logger initialized");
+	
     auto* app = CatEngine::CreateApp();
     app->Run();
     delete app;
